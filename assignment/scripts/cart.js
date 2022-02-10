3ef53d49325d059badd2b838199094117b0a175d
Andrew Lounsbury <lounsburya@gmail.com>
2022-02-10 17:12:28 -0600

    created empty function and tried 1st stretch

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,8 +1,8 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-
-let basket = []
+const maxItems = 5;
+let basket = [];
 function addItem(item){
   basket.push(item);
   return true;
@@ -25,6 +25,7 @@ function listItems(){
   //console.log(basket[i])
 //}
 //just making sure this works
+
 function empty(){
   let basket = []
   return basket;
@@ -32,3 +33,28 @@ function empty(){
 //return 'basket empty';
 empty()
 console.log('The basket is now empty', empty());
+
+
+// Stretch
+// put const maxItems at Topics
+
+// 2. Create a function called isFull(). It should:
+//   - return `false` if the basket contains *less* than max number of items
+//   - return `true` otherwise (equal or more than maxItems)
+
+//test 1
+// function isFull(){
+//   if (basket <= maxItems) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+//test 2
+// function isFull(){
+//   if (basket <= maxItems) {
+//     return true;
+//   }
+//
+// isFull()
+// console.log(isFull());
